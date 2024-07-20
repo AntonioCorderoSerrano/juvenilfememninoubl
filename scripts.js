f228a13dc736033010376d9bc3888cd75ecb5021
Antonio Cordero Serrano <152612563+AntonioCorderoSerrano@users.noreply.github.com>
2024-07-20 18:23:09 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -38,3 +38,67 @@ setInterval(autoSlide, 1800);
 document.addEventListener('DOMContentLoaded', () => {
     showSlide('galeria', 0);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll("header, section, footer");
+    const playerCards = document.querySelectorAll("#equipo .player-card");
+
+    function checkVisibility() {
+        elements.forEach(element => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                element.classList.add("visible");
+            }
+        });
+    }
+
+    function checkVisibilityCartas() {
+        playerCards.forEach(card => {
+            const rect = card.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom >= 0 && !card.classList.contains("animate")) {
+                card.classList.add("animate");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    window.addEventListener("resize", checkVisibility);
+
+    // Trigger the check once when the DOM is loaded
+    checkVisibility();
+    checkVisibilityCartas();
+});
+
+window.onload = function() {
+    // Agregar la clase fade-in al header y a la sección intro-section
+    document.querySelector('header').classList.add('fade-in');
+    document.querySelector('.intro-section').classList.add('fade-in');
+    // Agregar la clase hidden a todas las demás secciones
+    var sections = document.querySelectorAll('main section:not(.intro-section)');
+    sections.forEach(function(section) {
+        section.classList.add('hidden');
+    });
+    checkVisibility();
+};
+
+// Función para comprobar la visibilidad de los elementos
+function checkVisibility() {
+    var sections = document.querySelectorAll('main section');
+    var footer = document.querySelector('footer');
+    var windowHeight = window.innerHeight;
+
+    sections.forEach(function(section) {
+        var rect = section.getBoundingClientRect();
+        if (rect.top < windowHeight && section.classList.contains('hidden')) {
+            section.classList.remove('hidden');
+            section.classList.add('visible');
+        }
+    });
+
+    // Asegúrate de que el footer no tenga la clase 'hidden'
+    var rectFooter = footer.getBoundingClientRect();
+    if (rectFooter.top < windowHeight && footer.classList.contains('hidden')) {
+        footer.classList.remove('hidden');
+        footer.classList.add('visible');
+    }
+}
